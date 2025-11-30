1f200df3df579f4743b6faf7466b8d206a7b3414
Firas Elechi <firasse.el_echi@stud.tu-darmstadt.de>
2025-11-30 17:43:33 +0100

    save the highest score

--- snake-game/score_board.py
+++ snake-game/score_board.py
@@ -5,7 +5,9 @@ ALIGN =  "center"
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.high_score = 0
+        with open("data.txt", mode='r') as data:
+            self.high_score = int(data.read())
+
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
@@ -16,6 +18,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update()
 
