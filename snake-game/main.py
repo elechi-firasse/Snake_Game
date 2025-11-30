1a6123d5fe91e407cd993c553cd0043a304fbbbf
Firas Elechi <firasse.el_echi@stud.tu-darmstadt.de>
2025-11-30 17:05:28 +0100

    starting the game with pressing an arrow

--- snake-game/main.py
+++ snake-game/main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from turtle import Screen
 from food import Food
 from score_board import ScoreBoard
+
 screen = Screen()
 screen.setup(height=600, width=600)
 screen.bgcolor("black")
@@ -12,67 +13,65 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 score_board = ScoreBoard()
+game_is_on = False
+
+def start_up():
+    global game_is_on
+    game_is_on = True
+    snake.up()
+
+def start_down():
+    global game_is_on
+    game_is_on = True
+    snake.down()
+
+def start_left():
+    global game_is_on
+    game_is_on = True
+    snake.left()
+
+def start_right():
+    global game_is_on
+    game_is_on = True
+    snake.right()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
 
-game_is_on = True
-while game_is_on:
+# Arrow keys: start the game AND set direction
+screen.onkey(start_up, "Up")
+screen.onkey(start_down, "Down")
+screen.onkey(start_left, "Left")
+screen.onkey(start_right, "Right")
+
+# Main loop always runs so key events work
+while True:
     screen.update()
     time.sleep(0.1)
+
+    if not game_is_on:
+        continue   # wait until an arrow key starts the game
+
     snake.move()
-    #detect eating food
+
+    # detect eating food
     if snake.head.distance(food) < food.wid:
         food.refresh()
         snake.extend()
-        #food.wid = food.resize()
+        # food.wid = food.resize()
         score_board.update()
 
-
-    #detect end game
-    if -300 > snake.head.xcor() or snake.head.xcor() > 300 or -300 >snake.head.ycor() or snake.head.ycor() > 300 :
+    # detect end game (wall collision)
+    if (
+        snake.head.xcor() < -300 or snake.head.xcor() > 300 or
+        snake.head.ycor() < -300 or snake.head.ycor() > 300
+    ):
         game_is_on = False
         score_board.game_over()
 
-    #detect any collision with tail
+    # detect any collision with tail
     for segment in snake.turtles[1:]:
-        # if head collides with any segments of the tail tigger game over
         if snake.head.distance(segment) < 10:
             game_is_on = False
             score_board.game_over()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 screen.exitonclick()
